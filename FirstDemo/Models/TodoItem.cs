dfc31fa905af7639426c0c63df4b8962bc68106d
郭文武 <guowenwu1981815@163.com>
2019-04-30 22:51:22 +0800

    修改项目

--- FirstDemo/Models/TodoItem.cs
+++ FirstDemo/Models/TodoItem.cs
@@ -8,9 +8,14 @@ namespace FirstDemo.Models
     public class TodoItem
     {
         public long Id { get; set; }
-
+        /// <summary>
+        /// 姓名
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// 是否完成
+        /// </summary>
         public bool IsComplete { get; set; }
 
     }
